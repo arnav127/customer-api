a9f770022ee1204e6cb56ef21c4208190f57cb54
Arnav Dixit <arnav@appointy.com>
2021-05-14 15:34:19 +0530

    Add searchCustomer functionality
    - Add parameter check
    - Search slice for user
    - Return user

--- searchCustomer.go
+++ searchCustomer.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func searchCustomer(response http.ResponseWriter, request *http.Request) {
-	//TODO: search for customer
-	fmt.Println("search")
+	response.Header().Set("content-type", "application/json")
+	if request.Method != "GET" {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		response.Write([]byte(`{ "error": "Method not allowed" }`))
+		return
+	}
+	searchByEmail := request.URL.Query()["email"]
+
+	if searchByEmail != nil {
+		for _, user := range users {
+			if strings.ToLower(user.Email) == strings.ToLower(searchByEmail[0]) {
+				json.NewEncoder(response).Encode(user)
+				return
+			}
+		}
+	}
+	searchByFirstName := request.URL.Query()["first_name"]
+	if searchByFirstName != nil {
+		for _, user := range users {
+			if strings.ToLower(user.FirstName) == strings.ToLower(searchByFirstName[0]) {
+				json.NewEncoder(response).Encode(user)
+				return
+			}
+		}
+	}
+	response.WriteHeader(http.StatusNotFound)
+	response.Write([]byte(`{"error": "Record not found"}`))
 }

2ee7f2f5554f4da8839eafce04f8bbebfbb41868
Arnav Dixit <arnav@appointy.com>
2021-05-14 16:53:22 +0530

    Implement updateCustomer
    - Update customer by ID
    - Search for user by id and replace by updated user

--- updateCustomer.go
+++ updateCustomer.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func updateCustomer(response http.ResponseWriter, request *http.Request) {
-	//TODO: update customer in map
-	fmt.Println("Update")
+	response.Header().Set("content-type", "application/json")
+	if request.Method != "PUT"  {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		response.Write([]byte(`{ "error": "Method not allowed" }`))
+		return
+	}
+
+	var updateUser User
+	if err := json.NewDecoder(request.Body).Decode(&updateUser); err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte(`{ "error": "Bad request" }`))
+		return
+	}
+	responseEncoder := json.NewEncoder(response)
+	if request.Method == "PUT" {
+		for idx, _ := range users {
+			if users[idx].Id == updateUser.Id {
+				users[idx] = updateUser
+				responseEncoder.Encode(users[idx])
+				return
+			}
+		}
+	}
+
+	users = append(users, updateUser)
+	responseEncoder.Encode(updateUser)
 }

86f9b5cc46d4a46c8df4f258c076ac56a4c80e5a
Arnav Dixit <arnav@appointy.com>
2021-05-14 14:56:44 +0530

    Add user creation functionality

--- createCustomer.go
+++ createCustomer.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func createCustomer(response http.ResponseWriter, request *http.Request) {
-	//TODO: add customer to map
-	fmt.Println("Creating Customer")
+
+	if request.Method != "POST" {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		response.Write([]byte(`{ "error": "Method not allowed" }`))
+		return
+	}
+
+	// decode and check for error
+	var newUser User
+	if err := json.NewDecoder(request.Body).Decode(&newUser); err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte(`{ "error": "Bad request" }`))
+		return
+	}
+
+	//TODO: validate user before adding to map
+	//validate(newUser)
+
+	//add user to map
+	users[newUser.Id] = newUser
+	fmt.Println("Creating customer", newUser)
+
 }

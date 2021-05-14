c8082718a239af32d3bc6a3482b5a02404502f8b
Arnav Dixit <arnav@appointy.com>
2021-05-14 16:45:20 +0530

    Implement deleteCustomer
    - Delete customer by id
    - Slice updation by following algorithm:
      - Swap user to delete with last user of slice
      - Decrease slice length by 1

--- deleteCustomer.go
+++ deleteCustomer.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func deleteCustomer(response http.ResponseWriter, request *http.Request) {
-	//TODO: remove customer record
-	fmt.Println("Delete")
+	response.Header().Set("content-type", "application/json")
+	if request.Method != "GET" {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		response.Write([]byte(`{ "error": "Method not allowed" }`))
+		return
+	}
+	deleteID := request.URL.Query()["id"]
+	if deleteID != nil {
+		for idx, user := range users {
+			if user.Id == deleteID[0] {
+				totalUsers := len(users)
+				users[totalUsers-1], users[idx] = users[idx], users[totalUsers-1]
+				users = users[:totalUsers-1]
+				response.WriteHeader(http.StatusNoContent)
+				return
+			}
+		}
+	}
+	response.WriteHeader(http.StatusNotFound)
+	response.Write([]byte(`{ "error": "Customer does not exist" }`))
+
 }

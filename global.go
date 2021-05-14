bc6c9c6f713c65a3c3da2b13b01eb0215868be1a
Arnav Dixit <arnav@appointy.com>
2021-05-14 18:07:17 +0530

    Implement User validation
    - Valid email against regex
    - Name length between minNameLength and maxNameLength
    - Phone number should be 10 digits
    - Id cannot be empty

--- global.go
+++ global.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"regexp"
+)
+
 // User Struct to store user details
 type User struct {
 	Id        string `json:"id"`
@@ -11,3 +16,26 @@ type User struct {
 
 // users slice to store all users
 var users []User
+
+// validate : check if the user struct received is valid against set params
+func validate(user User) error {
+	const minNameLength, maxNameLength = 3, 20
+	const emailRegexString = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+	var emailRegex = regexp.MustCompile(emailRegexString)
+	if len(user.Email) < 5 || !emailRegex.MatchString(user.Email) {
+		return errors.New("invalid email")
+	}
+	if len(user.FirstName) < minNameLength || len(user.FirstName) > maxNameLength {
+		return errors.New("first_name should be between 3 and 20 characters")
+	}
+	if len(user.LastName) < minNameLength || len(user.LastName) > maxNameLength {
+		return errors.New("last_name should be between 3 and 20 characters")
+	}
+	if user.Phone < 1000000000 || user.Phone > 9999999999 {
+		return errors.New("invalid phone no")
+	}
+	if user.Id == "" {
+		return errors.New("id cannot be empty")
+	}
+	return nil
+}

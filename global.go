b5b6b23a4315de90188fe21abfe667c212e0d5f0
Arnav Dixit <arnav@appointy.com>
2021-05-15 09:45:09 +0530

    Update validate function to include allowEmpty parameter

--- global.go
+++ global.go
@@ -18,24 +18,39 @@ type User struct {
 var users []User
 
 // validate : check if the user struct received is valid against set params
-func validate(user User) error {
+func validate(user User, allowEmpty bool) error {
 	const minNameLength, maxNameLength = 3, 20
 	const emailRegexString = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
 	var emailRegex = regexp.MustCompile(emailRegexString)
-	if len(user.Email) < 5 || !emailRegex.MatchString(user.Email) {
-		return errors.New("invalid email")
+
+	if !(allowEmpty && user.Email == "") {
+		if len(user.Email) < 5 || !emailRegex.MatchString(user.Email) {
+			return errors.New("invalid email")
+		}
 	}
-	if len(user.FirstName) < minNameLength || len(user.FirstName) > maxNameLength {
-		return errors.New("first_name should be between 3 and 20 characters")
+
+	if !(allowEmpty && user.FirstName == "") {
+		if len(user.FirstName) < minNameLength || len(user.FirstName) > maxNameLength {
+			return errors.New("first_name should be between 3 and 20 characters")
+		}
 	}
-	if len(user.LastName) < minNameLength || len(user.LastName) > maxNameLength {
-		return errors.New("last_name should be between 3 and 20 characters")
+
+	if !(allowEmpty && user.LastName == "") {
+			if len(user.LastName) < minNameLength || len(user.LastName) > maxNameLength {
+			return errors.New("last_name should be between 3 and 20 characters")
+		}
 	}
-	if user.Phone < 1000000000 || user.Phone > 9999999999 {
-		return errors.New("invalid phone no")
+
+	if !(allowEmpty && user.Phone == 0) {
+		if user.Phone < 1000000000 || user.Phone > 9999999999 {
+			return errors.New("invalid phone no")
+		}
 	}
-	if user.Id == "" {
-		return errors.New("id cannot be empty")
+
+	if !(allowEmpty && user.Id == "") {
+		if user.Id == "" {
+			return errors.New("id cannot be empty")
+		}
 	}
 	return nil
 }

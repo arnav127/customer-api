95e26f9c4d9fabea6e3a7cedd637820408c1cbd1
Arnav Dixit <arnav@appointy.com>
2021-05-16 18:58:16 +0530

    Establish database connection
    Connect to postgres database

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	//connect to the database
+	connStr := genConnectionString()
+	fmt.Println(connStr)
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
 
 	//v1 api endpoints
 	http.HandleFunc("/v1/users/create", createCustomer)
@@ -25,4 +37,13 @@ func main() {
 		panic(err)
 		return
 	}
+}
+
+func genConnectionString() string {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("PORT")
+	sslmode := os.Getenv("SSLMODE")
+	return fmt.Sprintf("user=%v password=%v dbname=%v port=%v sslmode=%v", user, password, dbname, port, sslmode)
 }
